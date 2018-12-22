ed274542d607f308a6404463f4baf1a780aa274b
tatecarson <tatecarson@gmail.com>
2018-12-21 21:54:07 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <hr>
+    <app-servers></app-servers>
+    <server-details></server-details>
+    <hr>
+    <server-managed></server-managed>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Servers from "./components/Servers.vue";
+import ServerDetails from "./components/ServerDetails.vue";
+import ServerManaged from "./components/ServerManaged.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-servers": Servers,
+    "server-details": ServerDetails,
+    "server-managed": ServerManaged
+  }
+};
+</script>
+
---
+++ src/components/ServerDetails.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
